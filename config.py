7dc5d1eda0dbacc70105b330d4cc13618a632a60
ndthong2411 <74914277+ndthong2411@users.noreply.github.com>
2023-11-13 23:42:50 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+seed = 42
+batch_size = 16
+end_epoch = 110     
+init_lr = 0.001
+lr_milestones = [15, 30, 45, 60]
+lr_decay_rate = 0.1
+weight_decay = 1e-4
+input_size = 224
+config = 2
+# root = r'W:\AISIA\ip102_v1.1'
+# model_path = r'W:\COTA-master\checkpoint'
+# dataset_path = r'W:\AISIA\ip102_v1.1\images'
+
+root = r'E:/code/THESIS/printer_source'
+model_path = r'E:/code/THESIS/printer_source/checkpoint'
+dataset_path = r'E:/code/THESIS/printer_source/w0.xlsx'
